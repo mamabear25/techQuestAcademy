57110aae8610ae2cde17f278caf0e1d3742a345a
dubem0 <dubemagbakoba@icloud.com>
2023-02-16 13:29:33 +0100

    new endpoint added

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
     res.send("This is the homepage")
 })
 
+app.get("/faqpage", (req, res) => {
+    res.send("1. Kileleyi?", "2.How do i debug", "3.Is this playing?")
+})
+
 app.listen(port, () => {
     console.log(`server is listening on http://localhost:${port}`)
 })
