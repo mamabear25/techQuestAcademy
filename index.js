1dd31be2567b6eb3ab29d8e352bc7d7ef5b8ce8e
Daniel Sinjen <sinjendaniel@yahoo.com>
2023-02-16 13:30:22 +0100

    classwork

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.get("/", (req, res) => {
     res.send("This is the homepage")
 })
 
+app.get("/location", (req, res) => {
+    res.send("Lagos Nigeria")
+})
 app.listen(port, () => {
     console.log(`server is listening on http://localhost:${port}`)
 })
