1cbe87e317e1f36869fb1673fda9d0056b81d896
Odum Tobe <odumtobe@gmail.com>
2023-02-16 04:29:11 -0800

    added new endpoint

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
     res.send("This is the homepage")
 })
 
+app.get("/Contact Us", (req, res) => {
+    res.send("phone Num", "address", "email")
+})
+
 app.listen(port, () => {
     console.log(`server is listening on http://localhost:${port}`)
 })
